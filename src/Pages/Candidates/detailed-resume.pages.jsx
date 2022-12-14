0f5da8d1a31efece0d292390eefe2e306a395af7
gregaid427 <gregoryd427@gmail.com>
2022-12-14 18:45:26 +0000

    resumedetail

--- src/Pages/Candidates/detailed-resume.pages.jsx
+++ src/Pages/Candidates/detailed-resume.pages.jsx
@@ -22,18 +22,51 @@ function DetailedResume(props) {
   return (
     <>
       <Nav />
-      <div className="py-9 wrapper bg-[#f2f2f2]">
+
+      <div className="wrapper bg-[#f2f2f2]  ">
+        <div className="flex flex-row justify-between md:py-10 sm:py-4  gap-8">
+          <div className=" sm:justify-center font-semibold gap-2 text-4xl flex">
+            <h3 className="">Browse CVs</h3>
+          </div>
+          <div className=" sm:justify-center  justify-between font-semibold gap-2  flex">
+          <button className="p-2   flex uppercase   h-fit  rounded-md bg-[#69C080] ">
+          <IconPack1
+            className="h-6  "
+            style={{ stroke: "#ffffff", fill: "#ffffff" }}
+          />   <h4 className="  text-md text-white  ">Print</h4>
+            </button>
+            <button className="p-2   flex uppercase   h-fit  rounded-md bg-[#69C080] ">
+            <IconPack1
+            className="h-6  "
+            style={{ stroke: "#ffffff", fill: "#ffffff" }}
+          />  <h4 className="  text-md text-white  ">Save</h4>
+            </button>
+            <button className="p-2  flex uppercase   h-fit  rounded-md bg-black ">
+            <IconPack1
+            className="h-6  "
+            style={{ stroke: "#ffffff", fill: "#ffffff" }}
+          /> <h4 className="  text-md text-white  ">Back</h4>
+            </button>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="py-9 wrapper ">
         <div className=" my-5 flex flex-row md:gap-8 sm:gap-2 py-9 sm:py-3  ">
           <img
             className="md:w-4/12 sm:w-6/12 h-[300px] rounded-xl "
             src={SmilingWoman}
           ></img>
           <div className="sm:w-6/12 md:w-8/12  flex flex-col md:justify-around sm:justify-center ">
-            <h3 className="font-semibold text-3xl  ">Applicant Name</h3>
-            <h3 className="font-semibold text-xl text-slate-500 ">
-              Accra, Ghana
+            <h3 className="font-semibold text-4xl text-[#69C080] ">
+              Christianne Mensah
             </h3>
-            <h3 className="font-semibold text-xl sm:hidden ">
+            <h3 className="font-semibold text-xl ">ACCOUNTANT</h3>
+            <h3 className="font-semibold text-xl text-slate-500 ">
+              Open to: Financial Consulting, Accounting, Auditing
+            </h3>
+            <h3 className="text-xl  sm:hidden ">
               Bio statement goes here. Lorem ipsum dolor sit amet, consectetur
               adipiscing elit. Sed quis viverra diam. Nullam mauris eros,
               lacinia id nunc et, malesuada scelerisque urna. Mauris eget erat
@@ -53,24 +86,24 @@ function DetailedResume(props) {
 
       <div className=" justify-between border md:flex-row sm:flex-col  w-full wrapper   my-5 flex">
         <div className="flex  l items-center gap-2 py-4">
-        <IconPack1
-                className="h-6  "
-                style={{ stroke: "#69C080", fill: "#69C080" }}
-              />
+          <IconPack1
+            className="h-6  "
+            style={{ stroke: "#69C080", fill: "#69C080" }}
+          />
           <h3 className="text-xl font-semibold">+233 (0) 244 567 890</h3>{" "}
         </div>
         <div className="flex l items-center gap-2 py-4">
-        <IconPack1
-                className="h-6  "
-                style={{ stroke: "#69C080", fill: "#69C080" }}
-              />
+          <IconPack1
+            className="h-6  "
+            style={{ stroke: "#69C080", fill: "#69C080" }}
+          />
           <h3 className="text-xl font-semibold">cmensah@email.com</h3>
         </div>
         <div className="flex l items-center gap-2 py-4">
-        <IconPack2
-                className="h-6  "
-                style={{ stroke: "#69C080", fill: "#69C080" }}
-              />
+          <IconPack2
+            className="h-6  "
+            style={{ stroke: "#69C080", fill: "#69C080" }}
+          />
           <h3 className="text-xl font-semibold">
             42 Green Ave, Osu, Accra, Ghana
           </h3>
@@ -150,8 +183,8 @@ function DetailedResume(props) {
           WORK EXPERIENCE
         </h3>
         <div className="flex flex-col gap-4">
-        <div className="flex sm:flex-col md:flex-row ">
-          <div className=" flex flex-col md:w-3/12 sm:w-12/12">
+          <div className="flex sm:flex-col md:flex-row ">
+            <div className=" flex flex-col md:w-3/12 sm:w-12/12">
               <h3 className="text-xl font-semibold">Company Name</h3>
               <h3 className="text-xl text-slate-400 font-semibold">
                 2008 - 2010
@@ -177,8 +210,8 @@ function DetailedResume(props) {
         </div>
 
         <div className="flex flex-col gap-4">
-        <div className="flex sm:flex-col md:flex-row ">
-          <div className=" flex flex-col md:w-3/12 sm:w-12/12">
+          <div className="flex sm:flex-col md:flex-row ">
+            <div className=" flex flex-col md:w-3/12 sm:w-12/12">
               <h3 className="text-xl font-semibold">Company Name</h3>
               <h3 className="text-xl text-slate-400 font-semibold">
                 2008 - 2010
