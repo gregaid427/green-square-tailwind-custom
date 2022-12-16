8bf94a883f931efa1fa81804321a816a859b0874
gregaid427 <gregoryd427@gmail.com>
2022-12-16 14:42:31 +0000

    grad school year

--- src/Pages/Home/home.page.jsx
+++ src/Pages/Home/home.page.jsx
@@ -2,12 +2,12 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import GreenSquareLogo from "./../../Assets/images/green_square_logo.png";
 import Background from "./../../Assets/images/background1.jpg";
-// import { UserContext } from "../../Context/auth.context";
-// import { color } from "@mui/system";
+import { UserContext } from "../../Context/auth.context";
+
 
 function Home(props) {
   //   props.setShowNavBar(false);
-  //   const { setIsCompany } = useContext(UserContext);
+    const { setIsCompany } = useContext(UserContext);
 
   return (
     <><div className={`justify-center  bg-no-repeat bg-cover bg-center md:h-screen sm:h-max
@@ -22,20 +22,20 @@ function Home(props) {
           <div className="flex sm:justify-center md:justify-start " >  <img src={GreenSquareLogo} className=" w-[200px]   " /></div>
         
           <div className="  md:mt-[170px] sm:mt-20 " >
-          <div className="flex justify-center flex-col items-center gap-7  my-auto ">
-            <h3 className="md:text-7xl sm:text-5xl text-white">It all starts here.</h3>
+          <div className="flex justify-center flex-col items-center md:gap-7 sm:gap-3ss my-auto ">
+            <h3 className="md:text-7xl sm:text-2xl text-center text-white">It all starts here.</h3>
             <div className="grid grid-cols-2 md:grid-flow-col md:w-8/12 sm:w-12/12 gap-3  sm:flex sm:flex-col">
-              <div className="flex flex-col justify-between rounded-xl px-2 py-9 w-full row-span-1 bg-[#69C080] bg  content-center mx-auto  ">
+              <div className="flex flex-col justify-between rounded-xl px-2 md:py-9 sm:py-5 w-full row-span-1 bg-[#69C080] bg  content-center mx-auto  ">
                 <h4
                   
-                  className=" md:text-3xl sm:text-3xl text-center text-white"
+                  className=" md:text-3xl sm:text-2xl text-center text-white"
                 >
                   The career of <br /> your dreams.
                 </h4>{" "}
                 <Link  to="/company-guide" >
                 <div
                  
-                  className=" justify-end w-8/12 md:p-3 sm:p-2 mt-2 align-bottom  rounded-md text-3xl mx-auto  text-center fw-bold bg-[#FFBE24] text-black"
+                  className=" justify-end w-8/12 md:p-3 sm:p-2 mt-2 align-bottom  rounded-md md:text-3xl sm:text-2xl mx-auto  text-center fw-bold bg-[#FFBE24] text-black"
                   
                   // onClick={(e) => setIsCompany(false)}
                 >
@@ -43,17 +43,17 @@ function Home(props) {
                 </div>
                 </Link>
               </div>
-              <div className="flex flex-col justify-between rounded-xl px-2 py-9 w-full row-span-1 bg-[#69C080] bg  content-center mx-auto  ">
+              <div className="flex flex-col justify-between rounded-xl px-2 md:py-9 sm:py-5 w-full row-span-1 bg-[#69C080]   content-center mx-auto  ">
                 <h4
                   
-                  className="md:text-3xl sm:text-3xl text-4xl text-center text-white"
+                  className="md:text-3xl sm:text-2xl text-center text-white"
                 >
                  The team you've been<br></br>searching for
                 </h4>{" "}
                 <Link to="/employee-guide" >
                 <div
              
-                  className=" justify-end w-8/12 md:p-3 sm:p-2 p-4 mt-2 align-bottom  rounded-md text-3xl mx-auto  text-center fw-bold bg-[#FFBE24] text-black"
+                  className=" justify-end w-8/12 md:p-3 sm:p-2 p-4 mt-2 align-bottom  rounded-md md:text-3xl sm:text-2xl mx-auto  text-center fw-bold bg-[#FFBE24] text-black"
                   
                 // onClick={(e) => setIsCompany(true)}
                 >
